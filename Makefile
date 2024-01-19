18b14117fc6028542107eabe54d8fb50d13a6db0
Yassir El yaqine <yel-yaqi@e3r1p14.1337.ma>
2024-01-19 16:37:39 +0100

    finish ops

--- Makefile
+++ Makefile
@@ -6,14 +6,14 @@
 #    By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/01/17 17:42:48 by yel-yaqi          #+#    #+#              #
-#    Updated: 2024/01/19 15:43:31 by yel-yaqi         ###   ########.fr        #
+#    Updated: 2024/01/19 16:27:08 by yel-yaqi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = push_swap
 CFLAGS = -Wall -Wextra -Werror
 PARSING = parsing/verify.c parsing/exitf.c parsing/push.c parsing/satoi.c
-OPS = ops/swap.c ops/push.c ops/rotate.c
+OPS = ops/swap.c ops/push.c ops/rotate.c ops/rrotate.c
 
 all: $(NAME)
 $(NAME): main.c $(PARSING) $(OPS) push_swap.h
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 17:48:42 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/01/19 15:53:49 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/01/19 16:36:51 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,11 @@ void	push_b(t_list **stack_a, t_list **stack_b);
 
 void	rotate_a(t_list **stack_a, int mode);
 void	rotate_b(t_list **stack_b, int mode);
+void	rotate_ab(t_list **stack_a, t_list **stack_b);
+
+void	rrotate_a(t_list **stack_a, int mode);
+void	rrotate_b(t_list **stack_b, int mode);
+void	rrotate_ab(t_list **stack_a, t_list **stack_b);
 
 long	satoi(char *str);
 
