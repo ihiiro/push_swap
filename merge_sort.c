1a762c4465abfe9b75c7109d28dec560c63de9ef
Yassir El yaqine <yel-yaqi@e3r1p14.1337.ma>
2024-01-23 10:19:23 +0100

    correct push.c so that they remove the pushed elements from original stack

--- merge_sort.c
+++ merge_sort.c
@@ -6,24 +6,24 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 21:38:49 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/01/22 15:01:58 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/01/23 09:55:49 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	sorted(t_list *head)
-{
-	while (head && head->next)
-	{
-		if (head->integer > head->next->integer)
-			return (0);
-		head = head->next;
-	}
-	return (1);
-}
+// static int	sorted(t_list *head)
+// {
+// 	while (head && head->next)
+// 	{
+// 		if (head->integer > head->next->integer)
+// 			return (0);
+// 		head = head->next;
+// 	}
+// 	return (1);
+// }
 
-void	merge_sort(t_list **head)
-{
+// void	merge_sort(t_list **head)
+// {
 	
-}
+// }
--- ops/push.c
+++ ops/push.c
@@ -6,29 +6,36 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 15:01:52 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/01/22 15:06:32 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/01/23 10:17:57 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 #include <unistd.h>
 
+#include <stdio.h>
 void	push_a(t_list **stack_a, t_list **stack_b)
 {
+	t_list	*stack_b_next;
+
+	stack_b_next = (*stack_b)->next;
 	if (ft_lstsize(*stack_b))
 	{
 		ft_lstadd_front(stack_a, *stack_b);
-		*stack_a = (*stack_a)->next;
+		*stack_b = stack_b_next;
 	}
 	write(STDOUT_FILENO, "pa\n", 3);
 }
 
 void	push_b(t_list **stack_a, t_list **stack_b)
 {
+	t_list	*stack_a_next;
+
+	stack_a_next = (*stack_a)->next;
 	if (ft_lstsize(*stack_a))
 	{
 		ft_lstadd_front(stack_b, *stack_a);
-		*stack_b = (*stack_b)->next;
+		*stack_a = stack_a_next;
 	}
 	write(STDOUT_FILENO, "pb\n", 3);
 }
