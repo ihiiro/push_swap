8139649f21323829a0565ddbcac7c2466a250bd9
Yassir El yaqine <yel-yaqi@e3r1p12.1337.ma>
2024-02-07 13:39:08 +0100

    correct ops;

--- ops/push.c
+++ ops/push.c
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 15:01:52 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/02/06 18:53:26 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/02/07 13:29:13 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ void	push_a(t_list **stack_a, t_list **stack_b)
 {
 	t_list	*stack_b_next;
 
-	stack_b_next = (*stack_b)->next;
-	if (ft_lstsize(*stack_b))
+	if (!*stack_b)
 	{
-		ft_lstadd_front(stack_a, *stack_b);
-		*stack_b = stack_b_next;
+		write(STDOUT_FILENO, "pa\n", 3);
+		return ;
 	}
+	stack_b_next = (*stack_b)->next;
+	ft_lstadd_front(stack_a, *stack_b);
+	*stack_b = stack_b_next;
 	write(STDOUT_FILENO, "pa\n", 3);
 }
 
@@ -30,11 +32,13 @@ void	push_b(t_list **stack_a, t_list **stack_b)
 {
 	t_list	*stack_a_next;
 
-	stack_a_next = (*stack_a)->next;
-	if (ft_lstsize(*stack_a))
+	if (!*stack_a)
 	{
-		ft_lstadd_front(stack_b, *stack_a);
-		*stack_a = stack_a_next;
+		write(STDOUT_FILENO, "pb\n", 3);
+		return ;
 	}
+	stack_a_next = (*stack_a)->next;
+	ft_lstadd_front(stack_b, *stack_a);
+	*stack_a = stack_a_next;
 	write(STDOUT_FILENO, "pb\n", 3);
 }
--- ops/swap.c
+++ ops/swap.c
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 00:19:15 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/01/27 16:44:05 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/02/07 13:27:16 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,16 @@ void	swap_a(t_list **stack_a, int mode)
 {
 	t_list	*tmp;
 
-	if (ft_lstsize(*stack_a) > 1)
+	if (ft_lstsize(*stack_a) < 2)
 	{
-		tmp = *stack_a;
-		(*stack_a) = (*stack_a)->next;
-		tmp->next = (*stack_a)->next;
-		(*stack_a)->next = tmp;
+		if (mode)
+			write(STDOUT_FILENO, "sa\n", 3);
+		return ;
 	}
+	tmp = *stack_a;
+	(*stack_a) = (*stack_a)->next;
+	tmp->next = (*stack_a)->next;
+	(*stack_a)->next = tmp;
 	if (mode)
 		write(STDOUT_FILENO, "sa\n", 3);
 }
@@ -32,13 +35,16 @@ void	swap_b(t_list **stack_b, int mode)
 {
 	t_list	*tmp;
 
-	if (ft_lstsize(*stack_b) > 1)
+	if (ft_lstsize(*stack_b) < 2)
 	{
-		tmp = *stack_b;
-		(*stack_b) = (*stack_b)->next;
-		tmp->next = (*stack_b)->next;
-		(*stack_b)->next = tmp;
+		if (mode)
+			write(STDOUT_FILENO, "sb\n", 3);
+		return ;
 	}
+	tmp = *stack_b;
+	(*stack_b) = (*stack_b)->next;
+	tmp->next = (*stack_b)->next;
+	(*stack_b)->next = tmp;
 	if (mode)
 		write(STDOUT_FILENO, "sb\n", 3);
 }
