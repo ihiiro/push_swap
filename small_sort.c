fcdd3e866e00f4a25049927a968c5bda138461b0
Yassir El yaqine <yel-yaqi@e3r11p9.1337.ma>
2024-02-09 11:07:08 +0100

    optimized small_sort.c

--- small_sort.c
+++ small_sort.c
@@ -6,12 +6,22 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 17:37:01 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/02/08 16:27:29 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/02/09 10:56:18 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdlib.h>
+
+static void	rot_n(t_list **stack_a, int n, int rot)
+{
+	if (rot == 0)
+		while (n--)
+			rotate_a(stack_a, 1);
+	else
+		while (n--)
+			rrotate_a(stack_a, 1);
+}
 
 static void	sort_3(t_list **stack_a)
 {
@@ -23,8 +33,10 @@ static void	sort_3(t_list **stack_a)
 		max = (*stack_a)->next->integer;
 	if ((*stack_a)->next->next->integer > max)
 		max = (*stack_a)->next->next->integer;
-	while (max != (*stack_a)->next->next->integer)
-		rotate_a(stack_a, 1);
+	if ((*stack_a)->integer == max)
+		rot_n(stack_a, 1, 0);
+	else if ((*stack_a)->next->integer == max)
+		rot_n(stack_a, 1, 1);
 	if ((*stack_a)->integer > (*stack_a)->next->integer)
 		swap_a(stack_a, 1);
 }
@@ -42,8 +54,12 @@ static void	sort_4(t_list **stack_a, t_list **stack_b)
 			min = ptr->integer;
 		ptr = ptr->next;
 	}
-	while ((*stack_a)->integer != min)
-		rrotate_a(stack_a, 1);
+	if ((*stack_a)->next->integer == min)
+		rot_n(stack_a, 1, 0);
+	else if ((*stack_a)->next->next->integer == min)
+		rot_n(stack_a, 2, 0);
+	else if ((*stack_a)->next->next->next->integer == min)
+		rot_n(stack_a, 1, 1);
 	push_b(stack_a, stack_b);
 	sort_3(stack_a);
 	push_a(stack_a, stack_b);
@@ -62,8 +78,14 @@ static void	sort_5(t_list **stack_a, t_list **stack_b)
 			min = ptr->integer;
 		ptr = ptr->next;
 	}
-	while ((*stack_a)->integer != min)
-		rrotate_a(stack_a, 1);
+	if ((*stack_a)->next->integer == min)
+		rot_n(stack_a, 1, 0);
+	else if ((*stack_a)->next->next->integer == min)
+		rot_n(stack_a, 2, 0);
+	else if ((*stack_a)->next->next->next->integer == min)
+		rot_n(stack_a, 2, 1);
+	else if ((*stack_a)->next->next->next->next->integer == min)
+		rot_n(stack_a, 1, 1);
 	push_b(stack_a, stack_b);
 	sort_4(stack_a, stack_b);
 	push_a(stack_a, stack_b);
